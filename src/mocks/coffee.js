94566608c6cc3461f2b6f7d33ae7029aa1ccb968
J.C.Valgas <joaovitorcarvalhovalgas@gmail.com>
2022-05-01 15:33:14 -0300

    add coffee list

---
+++ src/mocks/coffee.js
@@ -0,0 +1,44 @@
+export const coffees = [
+  {
+    titulo: 'Metropolitan Express',
+    descricao: 'tempor incididunt ut labore et dolore magna aliqua.',
+    foto: '../src/images/large-coffee_ccexpress.png',
+    preco: 10,
+    sabor: 'Black Coffee',
+  },
+  {
+    titulo: 'Citzen Express',
+    descricao: 'tempor incididunt ut labore et dolore magna aliqua.',
+    foto: '../src/images/mug-coffee_ccexpress.png',
+    preco: 5,
+    sabor: 'Black Coffee',
+  },
+  {
+    titulo: 'Irish Coffee',
+    descricao: 'tempor incididunt ut labore et dolore magna aliqua.',
+    foto: '../src/images/coffee-with-cream2_ccexpress.png',
+    preco: 12,
+    sabor: 'Black Coffee',
+  },
+  {
+    titulo: 'Ristretto',
+    descricao: 'tempor incididunt ut labore et dolore magna aliqua.',
+    foto: '../src/images/simple-coffee_ccexpress.png',
+    preco: 15,
+    sabor: 'Black Coffee',
+  },
+  {
+    titulo: 'Caffè corretto',
+    descricao: 'tempor incididunt ut labore et dolore magna aliqua.',
+    foto: '/coffee-with-cream1_ccexpress.png',
+    preco: 8,
+    sabor: 'Black Coffee',
+  },
+  {
+    titulo: 'Caffè Latte',
+    descricao: 'tempor incididunt ut labore et dolore magna aliqua.',
+    foto: '../src/images/large-coffee_ccexpress.png',
+    preco: 6,
+    sabor: 'Black Coffee',
+  },
+];
