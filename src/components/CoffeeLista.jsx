fd598db291ddc74e7372535a781366cac75a849b
J.C.Valgas <joaovitorcarvalhovalgas@gmail.com>
2022-05-01 20:30:48 -0300

    feature add item

--- src/components/CoffeeLista.jsx
+++ src/components/CoffeeLista.jsx
@@ -1,18 +1,27 @@
 import './CoffeeLista.css';
 
 import {coffees} from '../mocks/coffee.js';
+import { useState } from 'react';
 
 function CoffeeLista() {
+
+  const [coffeeSelecionado, setCoffeeSelecionado] = useState({});
+  const adicionarItem = (coffeeIndex) => {
+    const coffee = {[coffeeIndex]: Number(coffeeSelecionado[coffeeIndex] || 0) + 1}
+    setCoffeeSelecionado({...coffeeSelecionado, ...coffee});
+  }
+
   return (
     <div className="CoffeeLista">
       {coffees.map((coffee, index) => 
       <div className="CoffeeListaItem" key={`CoffeeListaItem-${index}`}>
+        <span className="CoffeeListaItem__badge"> {coffeeSelecionado[index] || 0} </span>
         <div>
           <div className="CoffeeListaItem__titulo">{coffee.titulo}</div>
           <div className="CoffeeListaItem__preco">R$ {coffee.preco.toFixed(2)}</div>
           <div className="CoffeeListaItem__descricao">{coffee.descricao}</div>
           <div className="CoffeeListaItem__acoes Acoes">
-            <button className="Acoes__adicionar Acoes__adicionar--preencher">adicionar</button>
+            <button className="Acoes__adicionar Acoes__adicionar--preencher" onClick={() => adicionarItem(index)}>adicionar</button>
           </div>
         </div>
         <img className="CoffeeListaItem__foto" src={coffee.foto} />
