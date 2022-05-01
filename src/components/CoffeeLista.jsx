3f0528fe7fc815ddbebc3c475c94471ef6e662bf
J.C.Valgas <joaovitorcarvalhovalgas@gmail.com>
2022-05-01 19:26:25 -0300

    element rendering with map function

--- src/components/CoffeeLista.jsx
+++ src/components/CoffeeLista.jsx
@@ -1,21 +1,23 @@
-import './CoffeeLista.css'
+import './CoffeeLista.css';
 
-import 
+import {coffees} from '../mocks/coffee.js';
 
 function CoffeeLista() {
   return (
     <div className="CoffeeLista">
-      <div className="CoffeeListaItem">
+      {coffees.map((coffee, index) => 
+      <div className="CoffeeListaItem" key={`CoffeeListaItem-${index}`}>
         <div>
-          <div className="CoffeeListaItem__titulo">Citizen Express</div>
-          <div className="CoffeeListaItem__preco">R$ 10.00</div>
-          <div className="CoffeeListaItem__descricao">tempor incididunt ut labore et dolore magna aliqua.</div>
+          <div className="CoffeeListaItem__titulo">{coffee.titulo}</div>
+          <div className="CoffeeListaItem__preco">R$ {coffee.preco.toFixed(2)}</div>
+          <div className="CoffeeListaItem__descricao">{coffee.descricao}</div>
           <div className="CoffeeListaItem__acoes Acoes">
             <button className="Acoes__adicionar Acoes__adicionar--preencher">adicionar</button>
           </div>
         </div>
-        <img className="CoffeeListaItem__foto" src={require('../assets/images/large-coffee_ccexpress.png')} />
+        <img className="CoffeeListaItem__foto" src={coffee.foto} />
       </div>
+      )}
     </div>
   )
 }
